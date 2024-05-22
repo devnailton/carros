cde80265e3f0749b5dd7757735e65f9ee1cf1e71
Ubuntu <ubuntu@ip-172-31-29-148.ec2.internal>
2024-05-22 02:52:52 +0000

    Alterações necessárias

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/l-apey/Documents/Github/projeto_carros/venv"
+set -gx VIRTUAL_ENV "/home/ubuntu/projeto_carros/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
